de6f8e94fa520cb928cdec335234698bc5f847e9
Alexander Roman <monderasdor@gmail.com>
2025-11-28 15:22:49 +0200

    fixed cw PrintVersion refrences

--- cmd/cw/main.go
+++ cmd/cw/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -11,7 +10,11 @@ import (
 	"path/filepath"
 	"strings"
 
-	"codeberg.org/clockwise-lang/clockwise/cmd/cw/compiler"
+	cwcompiler "codeberg.org/clockwise-lang/clockwise/cmd/cw/compiler"
+	"codeberg.org/clockwise-lang/clockwise/checker"
+	"codeberg.org/clockwise-lang/clockwise/codegen"
+	"codeberg.org/clockwise-lang/clockwise/lexer"
+	"codeberg.org/clockwise-lang/clockwise/parser"
 )
 
 const (
@@ -47,8 +50,6 @@ func main() {
 		printUsage(nil)
 		os.Exit(0)
 	}
-
-	startTime := time.Now()
 
 	// Handle subcommands
 	switch os.Args[1] {
@@ -98,10 +99,10 @@ func buildCmd() {
 	}
 
 	// Run the compiler
-	compiler := NewCompiler(inputFile, *outputFile)
-	compiler.Verbose = *verbose
+	comp := cwcompiler.NewCompiler(inputFile, *outputFile)
+	comp.Verbose = *verbose
 
-	if err := compiler.Compile(); err != nil {
+	if err := comp.Compile(); err != nil {
 		log.Fatalf("Compilation failed: %v", err)
 	}
 }
@@ -128,10 +129,10 @@ func runCmd() {
 	tempExe := filepath.Join(os.TempDir(), "cwc-run-*"+filepath.Ext(inputFile))
 
 	// Compile to temp file
-	compiler := NewCompiler(inputFile, tempExe)
-	compiler.Verbose = *verbose
+	comp := cwcompiler.NewCompiler(inputFile, tempExe)
+	comp.Verbose = *verbose
 
-	if err := compiler.Compile(); err != nil {
+	if err := comp.Compile(); err != nil {
 		log.Fatalf("Compilation failed: %v", err)
 	}
 
@@ -228,202 +229,59 @@ func printUsage(err error) {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 	}
 	fmt.Fprintf(os.Stderr, "%s\n", appDescription)
-
-	if !write {
-		_, err = os.Stdout.Write(res)
-		return err
-	}
-
-	// Create a backup
-	if err := backupFile(filename, src); err != nil {
-		return fmt.Errorf("creating backup: %v", err)
-	}
-
-	// Write formatted content
-	return ioutil.WriteFile(filename, res, 0644)
 }
 
-func diff(b1, b2 []byte, filename string) (data []byte, err error) {
-	f1, err := writeTempFile("", "cwc-fmt-", b1)
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(f1)
-
-	f2, err := writeTempFile("", "cwc-fmt-", b2)
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(f2)
-
-	cmd := exec.Command("diff", "-u", f1, f2)
-	data, err = cmd.CombinedOutput()
-	if len(data) > 0 {
-		// diff exits with a non-zero status when the files don't match.
-		// Ignore that failure as long as we get output.
-		err = nil
-	}
-	return data, err
-}
-
-func writeTempFile(dir, prefix string, data []byte) (string, error) {
-	file, err := ioutil.TempFile(dir, prefix)
-	if err != nil {
-		return "", err
-	}
-	_, err = file.Write(data)
-	if err1 := file.Close(); err == nil {
-		err = err1
-	}
-	if err != nil {
-		os.Remove(file.Name())
-		return "", err
-	}
-	return file.Name(), nil
-}
-
-func backupFile(filename string, data []byte) error {
-	err := ioutil.WriteFile(filename+".bak", data, 0644)
-	if err != nil {
-		return fmt.Errorf("failed to create backup file: %v", err)
-	}
-	return nil
-}
-    fs := flag.NewFlagSet("build", flag.ExitOnError)
-    in := fs.String("in", "", "input .cw file")
-    out := fs.String("out", "a.out", "output exe")
-    keep := fs.Bool("keep", false, "keep temp build files")
-    fs.Parse(args)
-    if *in == "" {
-        fmt.Fprintln(os.Stderr, "-in required for build")
-        os.Exit(2)
-    }
-    if err := doBuild(*in, *out, *keep); err != nil {
-        fmt.Fprintln(os.Stderr, "build failed:", err)
-        os.Exit(1)
-    }
-}
-
-func runCmd(args []string) {
-    fs := flag.NewFlagSet("run", flag.ExitOnError)
-    in := fs.String("in", "", "input .cw file")
-    fs.Parse(args)
-    if *in == "" {
-        fmt.Fprintln(os.Stderr, "-in required for run")
-        os.Exit(2)
-    }
-    // build to a temp file and run
-    tmp := filepath.Join(os.TempDir(), "cw-run-"+filepath.Base(*in)+".exe")
-    if err := doBuild(*in, tmp, false); err != nil {
-        fmt.Fprintln(os.Stderr, "build failed:", err)
-        os.Exit(1)
-    }
-    cmd := exec.Command(tmp)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    cmd.Stdin = os.Stdin
-    if err := cmd.Run(); err != nil {
-        fmt.Fprintln(os.Stderr, "program exited with error:", err)
-        os.Exit(1)
-    }
-}
-
-func fmtCmd(args []string) {
-    fs := flag.NewFlagSet("fmt", flag.ExitOnError)
-    in := fs.String("in", "", "input .cw file")
-    write := fs.Bool("w", false, "write changes")
-    fs.Parse(args)
-    if *in == "" {
-        fmt.Fprintln(os.Stderr, "-in required for fmt")
-        os.Exit(2)
-    }
-    out, err := ioutil.ReadFile(*in)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "read error:", err)
-        os.Exit(1)
-    }
-    s := string(out)
-    s = strings.ReplaceAll(s, "\r\n", "\n")
-    lines := strings.Split(s, "\n")
-    for i, l := range lines {
-        lines[i] = strings.TrimRight(l, " \t")
-    }
-    res := strings.Join(lines, "\n")
-    if *write {
-        if err := ioutil.WriteFile(*in, []byte(res), 0644); err != nil {
-            fmt.Fprintln(os.Stderr, "write error:", err)
-            os.Exit(1)
-        }
-        fmt.Println("wrote", *in)
-    } else {
-        fmt.Print(res)
-    }
-}
-
-func initCmd(args []string) {
-    fs := flag.NewFlagSet("init", flag.ExitOnError)
-    name := fs.String("name", "clockwise-project", "project directory name")
-    fs.Parse(args)
-    if err := os.MkdirAll(*name, 0755); err != nil {
-        fmt.Fprintln(os.Stderr, "failed to create dir:", err)
-        os.Exit(1)
-    }
-    // create simple main.cw
-    example := "fn main() -> int {\n    Print(\"Hello Clockwise!\\n\")\n    return 0\n}\n"
-    if err := ioutil.WriteFile(filepath.Join(*name, "main.cw"), []byte(example), 0644); err != nil {
-        fmt.Fprintln(os.Stderr, "failed to write example:", err)
-        os.Exit(1)
-    }
-    fmt.Println("created project:", *name)
+func printVersion() {
+	fmt.Printf("%s version %s (commit %s, built %s)\n", appName, version, commit, date)
 }
 
 func doBuild(inPath, outPath string, keep bool) error {
-    src, err := ioutil.ReadFile(inPath)
-    if err != nil {
-        return err
-    }
-    tokens := lexer.New(string(src)).Tokenize()
-    p := parser.New(tokens)
-    program, perr := p.ParseProgram()
-    if perr != nil {
-        return perr
-    }
-    if err := checker.CheckProgram(program); err != nil {
-        return err
-    }
-    gosrc := codegen.Generate(program)
-    tmpDir, err := ioutil.TempDir("", "clockwise-build-")
-    if err != nil {
-        return err
-    }
-    if !keep {
-        defer os.RemoveAll(tmpDir)
-    }
-    mainFile := filepath.Join(tmpDir, "main.go")
-    if err := ioutil.WriteFile(mainFile, []byte(gosrc), 0644); err != nil {
-        return err
-    }
-    // merge runtime helpers into tmpDir (copy all files under runtime/* into tmpDir)
-    if info, err := os.Stat("runtime"); err == nil && info.IsDir() {
-        entries, err := os.ReadDir("runtime")
-        if err != nil {
-            return err
-        }
-        for _, e := range entries {
-            srcPath := filepath.Join("runtime", e.Name())
-            if err := copyDir(srcPath, tmpDir); err != nil {
-                return err
-            }
-        }
-    }
-    if err := ioutil.WriteFile(filepath.Join(tmpDir, "go.mod"), []byte("module cwtemp\n\n go 1.20\n"), 0644); err != nil {
-        return err
-    }
-    buildCmd := exec.Command("go", "build", "-o", outPath)
-    buildCmd.Dir = tmpDir
-    buildCmd.Stderr = os.Stderr
-    buildCmd.Stdout = os.Stdout
-    return buildCmd.Run()
+	src, err := ioutil.ReadFile(inPath)
+	if err != nil {
+		return err
+	}
+	tokens := lexer.New(string(src)).Tokenize()
+	p := parser.New(tokens)
+	program, perr := p.ParseProgram()
+	if perr != nil {
+		return perr
+	}
+	if err := checker.CheckProgram(program); err != nil {
+		return err
+	}
+	gosrc := codegen.Generate(program)
+	tmpDir, err := ioutil.TempDir("", "clockwise-build-")
+	if err != nil {
+		return err
+	}
+	if !keep {
+		defer os.RemoveAll(tmpDir)
+	}
+	mainFile := filepath.Join(tmpDir, "main.go")
+	if err := ioutil.WriteFile(mainFile, []byte(gosrc), 0644); err != nil {
+		return err
+	}
+	// merge runtime helpers into tmpDir (copy all files under runtime/* into tmpDir)
+	if info, err := os.Stat("runtime"); err == nil && info.IsDir() {
+		entries, err := os.ReadDir("runtime")
+		if err != nil {
+			return err
+		}
+		for _, e := range entries {
+			srcPath := filepath.Join("runtime", e.Name())
+			if err := copyDir(srcPath, tmpDir); err != nil {
+				return err
+			}
+		}
+	}
+	if err := ioutil.WriteFile(filepath.Join(tmpDir, "go.mod"), []byte("module cwtemp\n\n go 1.20\n"), 0644); err != nil {
+		return err
+	}
+	buildCmd := exec.Command("go", "build", "-o", outPath)
+	buildCmd.Dir = tmpDir
+	buildCmd.Stderr = os.Stderr
+	buildCmd.Stdout = os.Stdout
+	return buildCmd.Run()
 }
 
 // copyDir recursively copies a directory from src to dst (dst will be created).
